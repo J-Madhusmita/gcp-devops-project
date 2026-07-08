80fb01543e22483ab3f7571a0051f0fad08dd716
Madhusmita <mjena2818@gmail.com>
2026-07-09 01:41:27 +0530

    feat: deploy backend to GKE with Cloud SQL integration

--- terraform/iam.tf
+++ terraform/iam.tf
@@ -33,3 +33,12 @@ resource "google_project_iam_member" "pubsub_publisher" {
 
   member = "serviceAccount:${google_service_account.backend_sa.email}"
 }
+
+resource "google_project_iam_member" "artifact_registry_reader" {
+
+  project = var.project_id
+
+  role = "roles/artifactregistry.reader"
+
+  member = "serviceAccount:${google_service_account.backend_sa.email}"
+}
